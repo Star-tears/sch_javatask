2cab3bcf51b3f1f27574f52c799be0971fc3b475
Star-tears <1370443225@qq.com>
2021-12-06 19:07:45 +0800

    对之前的代码的一些小修改

--- Java程序设计实践/平时/Code1/src/work1/ShowSyntaxErrors.java
+++ Java程序设计实践/平时/Code1/src/work1/ShowSyntaxErrors.java
@@ -1,7 +1,15 @@
 package work1;
 
+//课本代码
+// public class ShowSyntaxErrors {
+//     public static main(String[] args){//
+//         System.out.println("Welcome to Java);
+//     }
+// }
+
+//正确代码
 public class ShowSyntaxErrors {
-    public static main(String[] args){
-        System.out.println("Welcome to Java);
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java");
     }
 }
